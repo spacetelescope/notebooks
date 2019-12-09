0d88b0f11cd917470f1f3a8e609b68786b3532da
Joseph Curtin <jcurtin@stsci.edu>
2019-12-09 15:24:44 +0900

    cleaned up implementation

--- .circleci/make_pages.py
+++ .circleci/make_pages.py
@@ -20,23 +20,6 @@ def main():
     logger.info(f'Added path[{repo_path}]')
     sys.path.append(repo_path)
     import convert
-    # args = make_parser().parse_args()
-    # # Used for debugging the .circleci pipeline
-    # # converted = [item for item in converted if not os.path.basename(item) in ['test-fail.html', 'test-succeed.html']]
-    # to_exclude = []
-    # if os.path.isfile('exclude_notebooks'):
-    #     with open('exclude_notebooks') as f:
-    #         for line in f:
-    #             if line.strip() != '':
-    #                 to_exclude.append(line.split('#')[0].strip())
-
-    # if to_exclude:
-    #     args.exclude = ','.join(to_exclude)
-    # 
-    # logger.info('Converting notebooks into HTML')
-    # converted = run_parsed('.', output_type='HTML', args=args)
-    # logger.info('Creating HTML Index')
-    # make_html_index(converted, './index.tpl')
 
 if __name__ in ['__main__']:
     main()
