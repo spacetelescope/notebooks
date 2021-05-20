0a55c77f5b0b803fc11b2061d337c913fa94eeb8
nkerman <nkerman@stsci.edu>
2021-05-20 15:14:54 -0400

    Fixed all of @eteq's comments: stylistic, Pathlib implementation.

--- notebooks/COS/AsnFile/test_asn.py
+++ notebooks/COS/AsnFile/test_asn.py
@@ -3,36 +3,37 @@
 ### From here, you can run the `calcos` pipeline on your new association file.
 ##### Running `calcos` is explained in *much* more detail in our [notebook on running the pipeline](https://github.com/spacetelescope/COS-Notebooks/blob/master/Calcos/Calcos.ipynb)
 
-##### The next two cells are only fully relavent if all of the following is true:
-##### 1. You are not on the Space Telescope Science Institute (STScI) internet, or connected to STScI via VPN.
-##### 2. You have not yet run the notebooks hosted in this repository on the `Calcos` pipeline *nor* the `DayNight` separation.
-
-##### In short, to run the `calcos` pipeline, you will need the relavent reference files. These will need be hosted in the directory assigned the environmetn variable `lref`. No matter *where* you place these files, you *must* create the lref environment variable.
+##### In short, to run the `calcos` pipeline, you will need the relavent reference files. These will need be hosted in the directory assigned the environmetn variable `lref`. 
+##### No matter *where* you place these files, you *must* create the lref environment variable.
 # %%
-import os
+import os, shutil
 import calcos
 from astropy.table import Table
 import matplotlib.pyplot as plt
+from pathlib import Path
 # %%
-data_dir = './data/'
-output_dir = './output/'
-plots_dir = output_dir + 'plots/'
+datadir = Path('./data/')
+outputdir = Path('./output/')
+plotsdir =  Path('./output/plots/')
 # %%
-os.environ['lref'] = '../../../calcos_reference/references/hst/cos'
-# %%
-!echo $lref
-# %%
-%%capture cap --no-stderr
+######### SETTING THE lref VARIABLE:
+### YOU LIKELY NEED TO CHANGE THIS LOCATION!!!
+where_i_keep_my_ref_files = "/grp/hst/cdbs/lref/"
+os.environ['lref'] = where_i_keep_my_ref_files
 
-!cp ./output/ldifcombo_2_asn.fits ./data/
-calcos.calcos('./data/ldifcombo_2_asn.fits', verbosity=0, outdir=output_dir+"./calcos_processed_1")
 # %%
-with open(output_dir+'output_calcos_1.txt', 'w') as f: # This file now contains the output of the last cell
-    f.write(cap.stdout)
+# shutil.copy("./output/ldifcombo_2_asn.fits", "./data/ldifcombo_2_asn.fits")
+# calcos.calcos('./data/ldifcombo_2_asn.fits', verbosity=0, outdir=str(outputdir/"./calcos_processed_1"))
 # %%
-processed_data_tab = Table.read(output_dir+'calcos_processed_1/'+'ldifcombo_x1dsum.fits')
+processed_data_tab = Table.read(str(outputdir/'calcos_processed_1/')+'/ldifcombo_x1dsum.fits')
 for segment in processed_data_tab:
     wvln, flux = segment["WAVELENGTH", "FLUX"]
     plt.plot(wvln, flux)
-plt.savefig("plots_dir"+"AsnFile_test.png", dpi = 200)
+    
+plt.xlabel('Wavelength [$\AA$]')
+plt.ylabel('Flux [ergs/s/$cm^2$/$\AA$]')
+
+plt.title("If this graph looks reasonable, your ASN file seems to have worked!\n")
+plt.tight_layout()
+plt.savefig(str(plotsdir/"AsnFile_test.png"), bbox_inches = 'tight', dpi = 200)
 # %%
